4eff2e971a359e22f17e876539272061b2adde77
Kiera Meredith <kiera@meredithkm.info>
2025-05-07 18:52:23 -0400

    Add Visual Studio Generated Template Project

---
+++ ModernWebForms/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace ModernWebForms.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ ModernWebForms/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ModernWebForms.Global" Language="C#" %>
