796d671a10bd278f31ee5f8a9968d0340654d2f9
Kiera Meredith <kiera@meredithkm.info>
2025-05-07 21:37:29 -0400

    Add .NET 8 Upgrade Project to Solution

---
+++ ModernWebForms.Core/Program.cs
@@ -0,0 +1,32 @@
+
+using System.Diagnostics;
+using System.Net;
+using Microsoft.AspNetCore.ResponseCompression;
+using Yarp.ReverseProxy.Forwarder;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddSystemWebAdapters();
+
+builder.Services.AddHttpForwarder();
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+app.UseAuthorization();
+app.UseSystemWebAdapters();
+
+app.MapDefaultControllerRoute();
+app.MapForwarder("/{**catch-all}", app.Configuration["ProxyTo"]).Add(static builder => ((RouteEndpointBuilder)builder).Order = int.MaxValue);
+
+app.Run();
