0dae1b96f92fadb04bf8cfbb6cb77b06c860aae7
Kiera Meredith <kiera@meredithkm.info>
2025-05-15 08:54:00 -0400

    Restructure Solution

--- Libraries/WebForms.Auth/Const.cs
+++ Libraries/WebForms.Auth/Const.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.Owin.Security;
-
-namespace WebForms.Helper;
+﻿namespace WebForms.Auth;
 
 public static class Const
 {
--- Libraries/WebForms.Auth/UserInfo.cs
+++ Libraries/WebForms.Auth/UserInfo.cs
@@ -6,7 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using WebForms.Helper.Enum;
 
-namespace WebForms.Helper.Auth;
+namespace WebForms.Auth;
 
 public static class UserInfo
 {
